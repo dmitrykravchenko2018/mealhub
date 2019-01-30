8e927b83daaf10526c090f9035392d8732b92dfa
dkravchenko <dmitrykravchenko95@gmail.com>
2019-01-30 20:36:11 +0200

    added web initialization and test controller

---
+++ src/main/java/org/mealhub/ApplicationInitializer.java
@@ -0,0 +1,27 @@
+package org.mealhub;
+
+import org.mealhub.config.SpringConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        final Class<?>[] servletConfigClasses = {
+                SpringConfiguration.class
+        };
+        return servletConfigClasses;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        final String[] servletMappings = {"/"};
+        return servletMappings;
+    }
+
+}
---
+++ src/main/java/org/mealhub/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.mealhub.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello");
+    }
+}
